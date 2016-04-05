0c41fb7e4abf781a8583c72801ae39468516cdb6
Gergely Kovacs <kovacsg@starschema.net>
2016-04-05 14:14:40 +0200

    modify p_cpu_usage_report_last_24_hours to really using the partitions...

--- views/p_cpu_usage_report_last_24_hours.sql
+++ views/p_cpu_usage_report_last_24_hours.sql
@@ -1,5 +1,5 @@
 create or replace view p_cpu_usage_report_last_24_hours as
 select * from p_cpu_usage_report
 where
-	cpu_usage_ts_rounded_15_secs >= now() - (interval'1 hour' * 24)
+	cpu_usage_ts_rounded_15_secs >= (now() - (interval'1 hour' * 24))::timestamp
 ;
