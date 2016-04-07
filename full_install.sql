83b0ca31062ec48fd796e55e04e8f59aad3d73bd
Gergely Kovacs <kovacsg@starschema.net>
2016-04-07 16:55:14 +0200

    Adding role creating to full install

--- full_install.sql
+++ full_install.sql
@@ -1,4 +1,14 @@
 \set ON_ERROR_STOP on
+create schema #schema_name#;
+create role readonly_#schema_name# with login password 'onlyread';
+create role palette_looker;
+create role palette_updater;
+CREATE ROLE palette_#schema_name#_looker;
+GRANT  palette_#schema_name#_looker TO palette_looker WITH ADMIN OPTION ;
+CREATE ROLE palette_#schema_name#_updater; 
+GRANT  palette_#schema_name#_updater TO palette_updater WITH ADMIN OPTION;
+grant palette_#schema_name#_looker to readonly_#schema_name#;
+
 set search_path = '#schema_name#';
 \i create_roles.sql
 \i db_version_meta.sql
