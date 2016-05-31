d4c2ee9f8f4ad95d11f2359fe60398f49940b139
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2016-05-31 17:49:34 +0200

    fix: use ls | sort -V for migration list

--- migrate.sh
+++ migrate.sh
@@ -6,10 +6,10 @@ ROOTDIR=`dirname $0`
 MIGRATIONS_DIR=${ROOTDIR}/migrations
 
 
-MIGRATION_VERSIONS="v1.1.7 v1.1.8 v1.1.9 v1.1.10 v1.1.11 v1.1.12 v1.1.13 v1.1.14 v1.1.15 v1.1.16 v1.1.17 v1.1.17.1"
+MIGRATION_VERSIONS=`ls ${MIGRATIONS_DIR} | sort -V`
 
 
-#EXISTING_VERSION="v1.1.14"
+#EXISTING_VERSION="v1.1.11"
 EXISTING_VERSION=`psql -d palette -c -t "select version_number from palette.db_version_meta limit 1;"`
 EXISTING_VERSION_IDX=`awk -v a="${MIGRATION_VERSIONS}" -v b="${EXISTING_VERSION}" 'BEGIN{print index(a,b)}'`
 
