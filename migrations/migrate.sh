9cc278fca8aa3b90224166658faa9497fb651e5a
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2016-06-01 14:06:43 +0200

    Added installer script

--- migrations/migrate.sh
+++ migrations/migrate.sh
@@ -11,15 +11,20 @@ MIGRATION_VERSIONS=`ls -d v* | sort -V`
 
 echo "All versions: [ ${MIGRATION_VERSIONS} ]"
 
-#EXISTING_VERSION_STR=" v1.1.13 "
-EXISTING_VERSION_STR=`psql -d palette -t -c "select version_number from palette.db_version_meta order by cre_date desc limit 1;"`
+EXISTING_VERSION_STR="v1.1.19"
+#EXISTING_VERSION_STR=`psql -d palette -t -c "select version_number from palette.db_version_meta order by cre_date desc limit 1;"`
 
 # Trim whitespace
 EXISTING_VERSION=${EXISTING_VERSION_STR//[[:blank:]]/}
 EXISTING_VERSION_IDX=`awk -v a="${MIGRATION_VERSIONS}" -v b="${EXISTING_VERSION}" 'BEGIN{print index(a,b)}'`
 
+if [[ $EXISTING_VERSION_IDX = 0 ]]; then
+  echo "Cannot find target version: ${EXISTING_VERSION} in versions"
+  exit 4
+  #statements
+fi
 
-echo "Found existing version: >>$EXISTING_VERSION<<"
+echo "Found existing version: >>$EXISTING_VERSION<< index: ${EXISTING_VERSION_IDX}"
 
 for VERSION in $MIGRATION_VERSIONS
 do
@@ -29,7 +34,7 @@ do
 
     cd ${VERSION}
 
-    psql -d palette -f "!install-up.sql"
+    #psql -d palette -f "!install-up.sql"
 
     cd ..
   fi
