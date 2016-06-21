9a7a17270af547e92349f63c770bc4f73869e555
Tari <501931446@LOGON.DS.GE.COM>
2016-06-21 11:47:15 +0200

    DATA-475 index p_id on p_serverlogs_report

--- migrations/v1.4.1/!install-up.sql
+++ migrations/v1.4.1/!install-up.sql
@@ -4,4 +4,4 @@ set role palette_palette_updater;
 
 \i 001-up-create_index_p_serverlogs_p_id_idx.sql
 
-insert into db_version_meta(version_number) values ('v1.3.8');
+insert into db_version_meta(version_number) values ('v1.4.1');
