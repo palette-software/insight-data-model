0af592d490c684bce5c5247f8fb9863c5a7df23a
Peter Ferenczy <ggpeti@gmail.com>
2016-06-24 21:39:32 +0200

    fix alter table syntax

--- migrations/v1.4.4/001-up-alter_s_cpu_usage_report.sql
+++ migrations/v1.4.4/001-up-alter_s_cpu_usage_report.sql
@@ -1,1 +1,1 @@
-ALTER TABLE s_cpu_usage_report DROP COLUMN user_type ADD COLUMN cpu_usage_user_type TEXT DEFAULT NULL;
+ALTER TABLE s_cpu_usage_report DROP COLUMN user_type, ADD COLUMN cpu_usage_user_type TEXT DEFAULT NULL;
