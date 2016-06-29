6d8ae4d45980cfff1d6a5ecc6ec7cfc775c10ec7
Gergely Kovacs <kovacsg@starschema.net>
2016-06-29 13:42:12 +0200

    Modify install for v1.5.2

--- migrations/v1.5.2/!install-up.sql
+++ migrations/v1.5.2/!install-up.sql
@@ -3,3 +3,5 @@ set search_path = '#schema_name#';
 set role palette_palette_updater;
 
 grant select on p_process_class_agg_report to palette_palette_looker;
+
+insert into db_version_meta(version_number) values ('v1.5.2');
