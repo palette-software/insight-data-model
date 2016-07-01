08547a8d2d9b50bf040f39de9c7c144af247c330
Peter Ferenczy <ggpeti@gmail.com>
2016-07-01 23:19:53 +0200

    drop child indexes in !install-up.sql

--- migrations/v1.8.0/!install-up.sql
+++ migrations/v1.8.0/!install-up.sql
@@ -7,6 +7,7 @@ BEGIN;
 \i 001-up-drop_p_interactor_cpu_usage_report.sql
 \i 002-up-alter_p_cpu_usage_report.sql
 \i 003-up-create_drop_child_indexes.sql
+SELECT drop_child_indexes('#schema_name#.p_cpu_usage_report_cpu_usage_vizql_session_idx');
 \i 004-up-replace_vizql_session_index.sql
 
 insert into db_version_meta(version_number) values ('v1.8.0');
--- migrations/v1.8.0/004-up-replace_vizql_session_index.sql
+++ migrations/v1.8.0/004-up-replace_vizql_session_index.sql
@@ -1,3 +1,2 @@
-SELECT drop_child_indexes('#schema_name#.p_cpu_usage_report_cpu_usage_vizql_session_idx');
 DROP INDEX p_cpu_usage_report_cpu_usage_vizql_session_idx;
 CREATE INDEX p_cpu_usage_report_cpu_usage_parent_vizql_session_idx ON p_cpu_usage_report (cpu_usage_parent_vizql_session);
