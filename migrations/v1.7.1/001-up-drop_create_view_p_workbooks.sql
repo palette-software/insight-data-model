a50d5e06b72e25a9f393062eae405d6f0ceddb03
Levente Dologh <ldologh@gmail.com>
2016-07-05 12:15:16 +0200

    fix drop function sqls

--- migrations/v1.7.1/001-up-drop_create_view_p_workbooks.sql
+++ migrations/v1.7.1/001-up-drop_create_view_p_workbooks.sql
@@ -1,1 +1,1 @@
-drop function if exists create_view_p_workbooks;
+drop function if exists create_view_p_workbooks(p_schema_name text);
--- migrations/v1.7.1/006-up-drop_create_view_p_datasources.sql
+++ migrations/v1.7.1/006-up-drop_create_view_p_datasources.sql
@@ -1,1 +1,1 @@
-drop function if exists create_view_p_datasources;
+drop function if exists create_view_p_datasources(p_schema_name text);
