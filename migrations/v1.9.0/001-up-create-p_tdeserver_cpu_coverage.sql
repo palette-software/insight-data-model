63f0d260a1350045dfedb634b5a1a06e55b0ea1a
Gyula Laszlo <gyula.laszlo.gm@gmail.com>
2016-07-07 15:28:46 +0200

    fix: add install-up to datamodel v1.9.0

---
+++ migrations/v1.9.0/001-up-create-p_tdeserver_cpu_coverage.sql
@@ -0,0 +1,29 @@
+-- Creates a view that gives us the coverage of tdeserver CPU consumption:
+-- it returns the ratio of (rows vizql_sessions / total rows) for p_cpu_report
+-- entries that have cpu usage, are from the 'tdeserver64' process.
+
+-- Example from USF:
+
+-- host  ts_hour   count_tdeserver_entries   tdeserver_fill_ratio
+-- [...]
+-- dataviz	2016-07-06 14:00:00.000000	1680	0
+-- dataviz	2016-07-06 20:00:00.000000	1584	19.76010101010101
+-- dataviz	2016-07-06 22:00:00.000000	1944	49.48559670781893
+-- dataviz	2016-07-06 23:00:00.000000	1886	37.75185577942736
+-- dataviz	2016-07-07 04:00:00.000000	1200	0
+-- [...]
+
+
+
+create or replace view test__tdeserver_vizql_session_coverage_report as
+  select
+    host_name,
+    date_trunc('hour', ts_rounded_15_secs) as ts_hour,
+    count(1) as count_tdeserver_entries_with_cpu_consumption,
+    ((sum(case when vizql_session IS NOT NULL then 1 else 0 end)::DOUBLE PRECISION /  count(1)::DOUBLE PRECISION) * 100.0) as tdeserver_fill_ratio
+    from palette.p_cpu_usage
+  where process_name='tdeserver64'
+     and ts_rounded_15_secs BETWEEN  ((now() - '48 hours'::interval) at time zone 'utc') and ((now() at time zone 'utc'))
+  group by host_name, date_trunc('hour', ts_rounded_15_secs)
+  order by ts_hour asc;
+
