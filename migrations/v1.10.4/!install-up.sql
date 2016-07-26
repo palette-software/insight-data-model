8e53211d61f6b8b850eb9b9d4defd97bc5e38dda
Levente Dologh <ldologh@gmail.com>
2016-07-26 17:30:34 +0200

    updated !install-up.sql

--- migrations/v1.10.4/!install-up.sql
+++ migrations/v1.10.4/!install-up.sql
@@ -11,13 +11,29 @@ alter table p_serverlogs_bootstrap_rpt rename column elapsed_seconds_to_bootstra
 alter table p_serverlogs_bootstrap_rpt add column session_duration double precision default 0;
 alter table p_serverlogs_bootstrap_rpt ALTER COLUMN session_elapsed_seconds TYPE double precision;
 
+alter table p_cpu_usage rename column start_ts to session_start_ts;
+alter table p_cpu_usage rename column end_ts to session_end_ts;
+alter table p_cpu_usage add column session_duration double precision default 0;
 
+alter table s_cpu_usage rename column start_ts to session_start_ts;
+alter table s_cpu_usage rename column end_ts to session_end_ts;
+alter table s_cpu_usage add column session_duration double precision default 0;
 
 drop view p_interactor_session_normal;
 alter table p_interactor_session ALTER COLUMN session_duration TYPE double precision using extract('epoch' from session_duration);
+alter table p_cpu_usage_report ALTER COLUMN session_duration TYPE double precision using extract('epoch' from session_duration);
+alter table s_cpu_usage_report ALTER COLUMN session_duration TYPE double precision using extract('epoch' from session_duration);
+
+select create_load_s_cpu_usage_report('palette');
 
 \i 001-up-insert_p_serverlogs_from_s_serverlogs.sql
 \i 002-up-p_interactor_session_normal.sql
+\i -up-load_s_cpu_usage_dataserver.sql
+\i -up-load_s_cpu_usage_rest.sql
+\i -up-load_s_cpu_usage_tabproto.sql
+\i -up-load_s_cpu_usage_tdeserver.sql
+\i -up-load_s_cpu_usage_vizql.sql
+\i -up-load_p_interactor_session.sql
 
 grant select on p_interactor_session_normal to palette_palette_looker;
 
