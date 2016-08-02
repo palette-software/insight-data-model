697e0d1398884e686e20a386cd723d809d99c2ee
Levente Dologh <ldologh@gmail.com>
2016-08-02 13:29:44 +0200

    add necessary things to table creation

--- tables/s_cpu_usage_dist_dims.sql
+++ tables/s_cpu_usage_dist_dims.sql
@@ -1,4 +1,4 @@
-CREATE TABLE s_cpu_usage_dist_dims AS 
+CREATE TABLE s_cpu_usage_dist_dims WITH (APPENDONLY=TRUE, ORIENTATION=COLUMN, COMPRESSTYPE=QUICKLZ) AS 
 select 
 		h_projects_p_id, 
 		h_sites_p_id, 
@@ -10,3 +10,10 @@ from
  		p_cpu_usage cpu
 where
 		1=2
+DISTRIBUTED BY (h_projects_p_id, 
+		h_sites_p_id, 
+		interactor_h_system_users_p_id, 
+		h_workbooks_p_id, 
+		publisher_h_users_p_id, 
+		publisher_h_system_users_p_id)
+		;
