d8aaa8f46536a1d8e2cc1fd210d3dea81c1e37aa
Levente Dologh <ldologh@gmail.com>
2016-08-02 13:53:25 +0200

    fix create to insert for new table in create_load

--- functions/create_load_s_cpu_usage_report.sql
+++ functions/create_load_s_cpu_usage_report.sql
@@ -143,8 +143,20 @@ begin
 
 							truncate table s_cpu_usage_dist_dims;
 																					
-							v_sql := ''create table s_cpu_usage_dist_dims as 
-										select distinct h_projects_p_id, h_sites_p_id, interactor_h_system_users_p_id, h_workbooks_p_id, publisher_h_users_p_id, publisher_h_system_users_p_id
+							v_sql := ''insert into s_cpu_usage_dist_dims 
+												(h_projects_p_id, 
+												h_sites_p_id, 
+												interactor_h_system_users_p_id, 
+												h_workbooks_p_id, 
+												publisher_h_users_p_id, 
+												publisher_h_system_users_p_id)
+										select 
+												distinct h_projects_p_id, 
+												h_sites_p_id, 
+												interactor_h_system_users_p_id, 
+												h_workbooks_p_id, 
+												publisher_h_users_p_id, 
+												publisher_h_system_users_p_id
 										from 	
 											p_cpu_usage cpu
 										where
