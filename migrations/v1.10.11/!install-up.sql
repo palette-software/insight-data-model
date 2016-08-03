3b7f3e89121d4d08e6ac089dcfa260fcca2a2afe
Levente Dologh <ldologh@gmail.com>
2016-08-03 11:11:33 +0200

    add migration

---
+++ migrations/v1.10.11/!install-up.sql
@@ -0,0 +1,22 @@
+\set ON_ERROR_STOP on
+set search_path = '#schema_name#';
+set role palette_palette_updater;
+
+BEGIN;
+
+\i 001-up-s_plainlogs_session_map.sql
+\i 002-up-s_serverlogs_spawner.sql
+\i 003-up-s_tde_filename_pids.sql
+\i 004-up-s_cpu_usage_dist_dims.sql
+\i 005-up-create_load_s_cpu_usage_report.sql
+\i 006-up-load_s_serverlogs_tdeserver.sql
+
+drop table if exists session_map
+drop table if exists t_s_spawner
+drop table if exists tde_filename_pids
+
+select create_load_s_cpu_usage_report('#schema_name#');
+
+insert into db_version_meta(version_number) values ('v1.10.11');
+
+COMMIT;
