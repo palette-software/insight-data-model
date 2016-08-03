0a820ecb6972e952d005c240fb11850ca87d1fc9
Levente Dologh <ldologh@gmail.com>
2016-08-03 11:03:35 +0200

    modify truncate command

--- functions/create_load_s_cpu_usage_report.sql
+++ functions/create_load_s_cpu_usage_report.sql
@@ -141,7 +141,9 @@ begin
 							execute v_sql_cur into v_max_ts_date;
 							v_max_ts_date := ''date'''''' || v_max_ts_date || '''''''';
 
-							truncate table s_cpu_usage_dist_dims;
+							v_sql := ''truncate table #schema_name#.s_tde_filename_pids''
+							v_sql := replace(v_sql, ''#schema_name#'', p_schema_name);
+							execute v_sql;
 																					
 							v_sql := ''insert into s_cpu_usage_dist_dims 
 												(h_projects_p_id, 
