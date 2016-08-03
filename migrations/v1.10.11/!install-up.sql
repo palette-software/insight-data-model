019b87eeaf09dd29b14d52eed3c0c0747ed9815c
Gergely Kovacs <kovacsg@starschema.net>
2016-08-03 14:52:20 +0200

    Add modified handle privileges to v1.10.11.

--- migrations/v1.10.11/!install-up.sql
+++ migrations/v1.10.11/!install-up.sql
@@ -10,6 +10,7 @@ BEGIN;
 \i 004-up-s_cpu_usage_dist_dims.sql
 \i 005-up-create_load_s_cpu_usage_report.sql
 \i 006-up-load_s_serverlogs_tdeserver.sql
+\i 007-up-handle_privileges.sql
 
 set role palette_etl_user;
 drop table if exists session_map;
@@ -18,7 +19,10 @@ drop table if exists tde_filename_pids;
 
 set role palette_palette_updater;
 select create_load_s_cpu_usage_report('#schema_name#');
+
+set role gpadmin;
 select handle_privileges('palette');
+
 
 insert into db_version_meta(version_number) values ('v1.10.11');
 
