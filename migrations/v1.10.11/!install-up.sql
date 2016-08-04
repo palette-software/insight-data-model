c376940f5cf71be7efb94db1d5635b35b6f6320e
Gergely Kovacs <kovacsg@starschema.net>
2016-08-04 10:49:34 +0200

    Modify order of commands in v1.10.11.

--- migrations/v1.10.11/!install-up.sql
+++ migrations/v1.10.11/!install-up.sql
@@ -1,8 +1,11 @@
 \set ON_ERROR_STOP on
 set search_path = '#schema_name#';
-set role palette_palette_updater;
 
 BEGIN;
+
+select handle_privileges('palette');
+
+set role palette_palette_updater;
 
 \i 001-up-s_plainlogs_session_map.sql
 \i 002-up-s_serverlogs_spawner.sql
@@ -12,17 +15,11 @@ BEGIN;
 \i 006-up-load_s_serverlogs_tdeserver.sql
 \i 007-up-handle_privileges.sql
 
-set role palette_etl_user;
 drop table if exists session_map;
 drop table if exists t_s_spawner;
 drop table if exists tde_filename_pids;
 
-set role palette_palette_updater;
 select create_load_s_cpu_usage_report('#schema_name#');
-
-set role gpadmin;
-select handle_privileges('palette');
-
 
 insert into db_version_meta(version_number) values ('v1.10.11');
 
