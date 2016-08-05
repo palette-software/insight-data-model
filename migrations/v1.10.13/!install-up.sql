20238255590023ff862c4b462b2c343192d578a5
Gergely Kovacs <kovacsg@starschema.net>
2016-08-05 10:40:46 +0200

    Add using clause to alter column.

--- migrations/v1.10.13/!install-up.sql
+++ migrations/v1.10.13/!install-up.sql
@@ -6,11 +6,11 @@ BEGIN;
 
 \i 001-up-load_p_threadinfo.sql
 
-alter table p_cpu_usage alter column parent_vizql_destroy_sess_ts type timestamp without time zone;
-alter table s_cpu_usage alter column parent_vizql_destroy_sess_ts type timestamp without time zone;
+alter table p_cpu_usage alter column parent_vizql_destroy_sess_ts type timestamp without time zone using to_timestamp(parent_vizql_destroy_sess_ts, 'yyyy-mm-dd HH24.MI.SS.MS');
+alter table s_cpu_usage alter column parent_vizql_destroy_sess_ts type timestamp without time zone using to_timestamp(parent_vizql_destroy_sess_ts, 'yyyy-mm-dd HH24.MI.SS.MS');
 
-alter table p_cpu_usage_report alter column cpu_usage_parent_vizql_destroy_sess_ts type timestamp without time zone;
-alter table s_cpu_usage_report alter column cpu_usage_parent_vizql_destroy_sess_ts type timestamp without time zone;
+alter table p_cpu_usage_report alter column cpu_usage_parent_vizql_destroy_sess_ts type timestamp without time zone using to_timestamp(parent_vizql_destroy_sess_ts, 'yyyy-mm-dd HH24.MI.SS.MS');
+alter table s_cpu_usage_report alter column cpu_usage_parent_vizql_destroy_sess_ts type timestamp without time zone using to_timestamp(parent_vizql_destroy_sess_ts, 'yyyy-mm-dd HH24.MI.SS.MS');
 
 insert into db_version_meta(version_number) values ('v1.10.13');
 
