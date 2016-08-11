d81e3ea1bee728bc3dcf086678c284c98c495735
Gergely Kovacs <kovacsg@starschema.net>
2016-08-11 15:05:21 +0200

    Add search_path and user perform in manage_partitions.

--- functions/manage_partitions.sql
+++ functions/manage_partitions.sql
@@ -2,10 +2,12 @@ CREATE or replace function manage_partitions(p_schema_name text, p_table_name te
 AS $$
 BEGIN
 		
+		execute 'set search_path = ' || p_schema_name;
+		
 		if p_table_name in ('threadinfo', 'serverlogs', 'p_serverlogs', 'p_threadinfo', 'p_cpu_usage', 'p_cpu_usage_report') then
-			select manage_multi_range_partitions(p_schema_name, p_table_name);
+			perform manage_multi_range_partitions(p_schema_name, p_table_name);
 		elseif p_table_name in ('p_cpu_usage_agg_report', 'p_interactor_session', 'p_process_class_agg_report', 'p_cpu_usage_bootstrap_rpt', 'p_serverlogs_bootstrap_rpt') then
-			select manage_single_range_partitions(p_schema_name, p_table_name);
+			perform manage_single_range_partitions(p_schema_name, p_table_name);
 		else raise notice '--WARNING: No partition management happened for % - wrong table specified?--', p_table_name;
 		end if;
 		
