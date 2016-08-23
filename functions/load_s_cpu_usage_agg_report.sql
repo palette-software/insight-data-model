862a014aef79c7ab3f365c16b9d9717ff71507be
Gergely Kovacs <kovacsg@starschema.net>
2016-08-23 11:35:46 +0200

    Rename load_p_cpu_usage_agg_report to stage.

--- functions/load_s_cpu_usage_agg_report.sql
+++ functions/load_s_cpu_usage_agg_report.sql
@@ -1,4 +1,4 @@
-CREATE or replace function load_p_cpu_usage_agg_report(p_schema_name text) returns bigint
+CREATE or replace function load_s_cpu_usage_agg_report(p_schema_name text) returns bigint
 AS $$
 declare
 	v_sql text;
@@ -9,18 +9,12 @@ BEGIN
 
 		execute 'truncate table palette.s_cpu_usage_agg_report';
 
-		execute 'set local search_path = ' || p_schema_name;				
+		execute 'set local search_path = ' || p_schema_name;
 
 		v_sql_cur := 'select to_char(coalesce(max(timestamp_utc)::date, date''1001-01-01''), ''yyyy-mm-dd'') from p_cpu_usage_agg_report';		
 		execute v_sql_cur into v_max_ts_date_p_cpu_usage_agg_report;
 		v_max_ts_date_p_cpu_usage_agg_report := 'date''' || v_max_ts_date_p_cpu_usage_agg_report || '''';
 				
-		v_sql_cur := 'delete from p_cpu_usage_agg_report where timestamp_utc::date >= #max_ts_date_p_cpu_usage_agg_report#';		
-		v_sql_cur := replace(v_sql_cur, '#max_ts_date_p_cpu_usage_agg_report#', v_max_ts_date_p_cpu_usage_agg_report);				
-
-		raise notice 'I: %', v_sql_cur;
-		execute v_sql_cur;
-
 		v_sql := 'insert into s_cpu_usage_agg_report 
 				(cpu_usage_host_name
 		       , cpu_usage_process_name
@@ -127,78 +121,17 @@ BEGIN
 		execute v_sql;
 		
 		GET DIAGNOSTICS v_num_inserted = ROW_COUNT;
-				
-		perform manage_partitions('palette', 'p_cpu_usage_agg_report');
-		
-		insert into p_cpu_usage_agg_report (
-		 		 cpu_usage_host_name
-		       , cpu_usage_process_name
-		       , timestamp_utc
-		       , workbook_name
-		       , interactor_s_user_id
-		       , interactor_s_user_name
-		       , interactor_s_user_name_id
-		       , interactor_s_user_friendly_name
-		       , interactor_s_user_friendly_name_id
-		       , interactor_s_user_email
-		       , publisher_s_user_email
-		       , publisher_s_user_id
-		       , publisher_s_user_name
-		       , publisher_s_user_friendly_name
-		       , publisher_s_user_name_id
-		       , publisher_s_user_friendly_name_id
-		       , publisher_user_site_id
-		       , workbook_id
-		       , workbook_name_id
-		       , workbook_revision
-		       , workbook_name_id_revision
-		       , site_name
-		       , site_id
-		       , project_name
-		       , project_id
-		       , project_name_id
-		       , site_name_id
-		       , site_project
-		       , cpu_usage_cpu_time_consumption_seconds
-		       , cpu_usage_cpu_time_consumption_minutes
-		       , cpu_usage_cpu_time_consumption_hours
-		       , vizql_session_count
-		)
-		SELECT  cpu_usage_host_name
-		       , cpu_usage_process_name
-		       , timestamp_utc
-		       , workbook_name
-		       , interactor_s_user_id
-		       , interactor_s_user_name
-		       , interactor_s_user_name_id
-		       , interactor_s_user_friendly_name
-		       , interactor_s_user_friendly_name_id
-		       , interactor_s_user_email
-		       , publisher_s_user_email
-		       , publisher_s_user_id
-		       , publisher_s_user_name
-		       , publisher_s_user_friendly_name
-		       , publisher_s_user_name_id
-		       , publisher_s_user_friendly_name_id
-		       , publisher_user_site_id
-		       , workbook_id
-		       , workbook_name_id
-		       , workbook_revision
-		       , workbook_name_id_revision
-		       , site_name
-		       , site_id
-		       , project_name
-		       , project_id
-		       , project_name_id
-		       , site_name_id
-		       , site_project
-		       , cpu_usage_cpu_time_consumption_seconds
-		       , cpu_usage_cpu_time_consumption_minutes
-		       , cpu_usage_cpu_time_consumption_hours
-		       , vizql_session_count
-		 FROM s_cpu_usage_agg_report;
+						
+                        
+        v_sql_cur := 'delete from p_cpu_usage_agg_report where timestamp_utc::date >= #max_ts_date_p_cpu_usage_agg_report#';
+		v_sql_cur := replace(v_sql_cur, '#max_ts_date_p_cpu_usage_agg_report#', v_max_ts_date_p_cpu_usage_agg_report);
 
-			
+		raise notice 'I: %', v_sql_cur;
+		execute v_sql_cur;
+
+		perform manage_partitions('palette', 'p_cpu_usage_agg_report');
+        v_num_inserted := ins_stage_to_dwh(p_schema_name, 'p_cpu_usage_agg_report');
+        
 		return v_num_inserted;
 END;
 $$ LANGUAGE plpgsql;
