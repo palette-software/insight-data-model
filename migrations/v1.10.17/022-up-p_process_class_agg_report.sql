086957aca40c2cb987722f2bfd5436ffbb4ba58d
Levente Dologh <ldologh@gmail.com>
2016-08-24 16:41:30 +0200

    fix wrong 022 file in migration

--- migrations/v1.10.17/022-up-p_process_class_agg_report.sql
+++ migrations/v1.10.17/022-up-p_process_class_agg_report.sql
@@ -1,6 +1,18 @@
-create table #schema_name#.s_process_class_agg_report 
+CREATE TABLE p_process_class_agg_report
+(
+  p_id                         BIGSERIAL,
+  ts_rounded_15_secs           TIMESTAMP,
+  process_name                 TEXT,
+  host_name                    TEXT,
+  cpu_usage_core_consumption   DOUBLE PRECISION,
+  cpu_usage_cpu_time_consumption_seconds DOUBLE PRECISION,
+  cpu_usage_memory_usage_bytes BIGINT,
+  tableau_process              BOOLEAN
+)
 WITH (APPENDONLY=TRUE, ORIENTATION=COLUMN, COMPRESSTYPE=QUICKLZ)
-as 
-select * from #schema_name#.p_process_class_agg_report where 1 = 2
-DISTRIBUTED BY (ts_rounded_15_secs);
-;
+DISTRIBUTED BY (p_id)
+PARTITION BY RANGE (ts_rounded_15_secs)
+(PARTITION "100101" START (date '1001-01-01') INCLUSIVE
+	END (date '1001-02-01') EXCLUSIVE
+WITH (appendonly=true, orientation=column, compresstype=quicklz)	
+);
