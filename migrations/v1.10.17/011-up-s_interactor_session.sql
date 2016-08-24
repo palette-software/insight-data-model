4461c403c63ccda31121811206d0a23d50ad3a0b
Levente Dologh <ldologh@gmail.com>
2016-08-24 16:26:27 +0200

    fix create table, delete #schema_name#

--- migrations/v1.10.17/011-up-s_interactor_session.sql
+++ migrations/v1.10.17/011-up-s_interactor_session.sql
@@ -1,6 +1,6 @@
-create table #schema_name#.s_interactor_session 
+create table s_interactor_session 
 WITH (APPENDONLY=TRUE, ORIENTATION=COLUMN, COMPRESSTYPE=QUICKLZ)
 as 
-select * from #schema_name#.p_interactor_session where 1 = 2
+select * from p_interactor_session where 1 = 2
 DISTRIBUTED BY (session_start_ts);
 ;
