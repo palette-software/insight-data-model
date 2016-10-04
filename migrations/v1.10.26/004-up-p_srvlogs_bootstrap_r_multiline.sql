cd8a9870cde4efd2aedf243249051dd9dd491df4
Gergely Kovacs <kovacsg@starschema.net>
2016-10-04 10:37:00 +0200

    Add process_name to p_srvlogs_bootstrap_r_multiline

--- migrations/v1.10.26/004-up-p_srvlogs_bootstrap_r_multiline.sql
+++ migrations/v1.10.26/004-up-p_srvlogs_bootstrap_r_multiline.sql
@@ -1,58 +1,32 @@
-drop view palette.p_srvlogs_bootstrap_r_multiline;
-
-create view palette.p_srvlogs_bootstrap_r_multiline as
-with t_base as
-(select 
+CREATE VIEW p_srvlogs_bootstrap_r_multiline AS 
+WITH t_base AS (
+    SELECT 
         p_id,
         ts,
         start_ts,
+        process_name, 
         parent_vizql_session,
         k,
-        string_to_array(v, '\\n ') as v_array,
-        array_upper(string_to_array(v, '\\n '), 1) as v_array_length
-from 
-    palette.p_serverlogs_bootstrap_rpt
-)
-
-select 
+        string_to_array(v, '\\n ') AS v_array,
+        array_upper(string_to_array(v, '\\n '), 1) AS v_array_length
+    FROM p_serverlogs_bootstrap_rpt
+    )    
+    
+SELECT 
     b.p_id,
-    g.i as v_line_id,
-    b.ts,
-    b.start_ts,
+    g.i AS line_num,
+    b.ts, 
+    b.start_ts, 
+    b.process_name, 
     b.parent_vizql_session,
-    b.k,
-    b.v_array,
-    b.v_array_length,
-    b.v_array[g.i] as v_one_line
-from
+    b.k, 
+    b.v_array[g.i] AS v_line_value 
+FROM 
     t_base b
-    inner join (select
-                    p_id,
-                    start_ts,
-                    generate_series(1, t.v_array_length) as i
-                from
-                    t_base t
-                ) g on (b.p_id = g.p_id and b.start_ts = g.start_ts)  
+    inner join (SELECT 
+                    t.p_id,
+                    t.start_ts, 
+                    generate_series(1, t.v_array_length) AS i
+                FROM 
+                    t_base t) g ON b.p_id = g.p_id AND b.start_ts = g.start_ts
 ;
-
-select *
-from
-(
-select * from palette.p_srvlogs_bootstrap_r_multiline
-where  start_ts > now()::date - 2
-) a
-limit 10
-;
-
-
-grant select on palette.p_srvlogs_bootstrap_r_multiline to palette_palette_looker;
-
-
-
-
-
-
-
-
-        
-        
