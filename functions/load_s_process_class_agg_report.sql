dee8580b43164b1a8b761e44796bc0748dd8a6f6
Gergely Kovacs <kovacsg@starschema.net>
2016-10-06 15:57:27 +0200

    Add inremental load for p_process_class_agg_report

--- functions/load_s_process_class_agg_report.sql
+++ functions/load_s_process_class_agg_report.sql
@@ -6,61 +6,20 @@ declare
 	v_sql text;
 	v_num_inserted bigint;
 	v_from text;
-	v_sql_cur text;
+	v_sql_cur text;    
 BEGIN
-    execute 'set local search_path = ' || p_schema_name;
 
+    execute 'set local search_path = ' || p_schema_name;
+    
     -- At first find the day that should be the next one loaded
     -- This means it is the last that has data either in the agg_report table or in cpu_usage_report
-	v_sql_cur := '
-	    select
-		    to_char(coalesce(
-			    max(ts_rounded_15_secs),
-				date''1001-01-01''), ''yyyy-mm-dd'')
-		from p_process_class_agg_report';
-	raise notice 'I: %', v_sql_cur;
+	v_sql_cur := 'select get_max_ts_date(''#schema_name#'', ''p_process_class_agg_report'')';
+    v_sql_cur := replace(v_sql_cur, '#schema_name#', p_schema_name);
 	execute v_sql_cur into v_from;
-	
-  	v_sql_cur := 'select load_s_process_class_agg_report(''#p_schema_name#'', ''#v_from#'')';
-	v_sql_cur := replace(v_sql_cur, '#p_schema_name#', p_schema_name);
-	v_sql_cur := replace(v_sql_cur, '#v_from#', v_from);
-	raise notice 'I: %', v_sql_cur;
-	execute v_sql_cur into v_num_inserted;
-	return v_num_inserted;
-END;
-$$ LANGUAGE plpgsql;
 
-
-CREATE or replace function load_s_process_class_agg_report(p_schema_name text, p_from text) returns bigint
-AS $$
-declare
-	v_sql text;
-	v_num_inserted bigint;
-	v_sql_cur text;
-    v_to text;
-    v_tableau_process_list text;
-BEGIN
-	execute 'set local search_path = ' || p_schema_name;
-
-    -- Get the upper limit of the current load iteration
-	v_sql_cur := 'select
-		to_char(coalesce(min(cpu_usage_ts_rounded_15_secs), date''#p_from#'' + 1), ''yyyy-mm-dd hh24:mi:ss.ms'')
-	from
-		p_cpu_usage_report
-	where
-        cpu_usage_thread_id = -1
-	 	and cpu_usage_ts_rounded_15_secs >= date''#p_from#'' + 1
-	';
-	v_sql_cur := replace(v_sql_cur, '#p_from#', p_from);
-	raise notice 'I: %', v_sql_cur;
-	execute v_sql_cur into v_to;
-
-	-- Loading with the same logic as delete. When everything is find we load only "today" but if we lagged
-    -- behind we load two days as that is needed for "progressing" and not getting stuck in a single day.
-	-- The plus 1 in the to clause is needed as we compare timestamp to date and in that case date is
-	-- implicit converted to timestamp at 00:00:00.000
-	v_sql := 'insert into s_process_class_agg_report
-	    (
+    v_sql := 
+    'insert into s_process_class_agg_report
+	    (   max_tho_p_id,
             ts_rounded_15_secs,
             process_name,
             host_name,
@@ -69,44 +28,73 @@ BEGIN
             cpu_usage_memory_usage_bytes,
             tableau_process
 		)
-	    select
-    	    cpu_usage_ts_rounded_15_secs,
+    select
+        max_tho_p_id,
+        ts_rounded_15_secs,
+        process_name,
+        host_name,
+        core_consumption,
+        cpu_time_consumption_seconds,
+        memory_usage_bytes,
+        tableau_process
+    from
+        (
+        select
+            max(tho.p_id) as max_tho_p_id,
+            tho.ts_rounded_15_secs,
             case
                 when pc.process_class = ''Tableau'' then pc.process_name
                 else coalesce(pc.process_class, ''Non-Tableau'')
             end as process_name,
-            cpu_usage_host_name,
-            sum(cpu_usage_cpu_core_consumption) as cpu_usage_core_consumption,
-            sum(cpu_usage_cpu_time_consumption_seconds) as cpu_usage_cpu_time_consumption_seconds,
-            sum(cpu_usage_memory_usage_bytes) as cpu_usage_memory_usage_bytes,
+            tho.host_name,
+            sum(tho.cpu_core_consumption) as core_consumption,
+            sum(tho.cpu_time_delta_ticks::numeric / 10000000) as cpu_time_consumption_seconds,
+            sum(tho.memory_usage_bytes) as memory_usage_bytes,
             case
                 when pc.process_class = ''Tableau'' then true
                 else false
             end as tableau_process
         from
-	        p_cpu_usage_report p_cpu
-        left outer join p_process_classification pc
-            on pc.process_name = p_cpu.cpu_usage_process_name
+            p_threadinfo_delta tho
+            left outer join p_process_classification pc on (pc.process_name = tho.process_name)
         where
-			cpu_usage_thread_id = -1
-			and cpu_usage_ts_rounded_15_secs >= date''#p_from#''
-			and cpu_usage_ts_rounded_15_secs <= timestamp''#v_to#''
+        	tho.thread_id = -1
+        	and tho.ts_rounded_15_secs >= date''#v_from#'' - 1
+            -- The last 15 seconds could have new records with the new p_threadinfo load so
+            -- we always skip the very recent 15 seconds in order to avoid "merge"
+            -- This way the max(tho.p_id) always be the same for the group by
+            and (host_name, ts_rounded_15_secs) not in 
+                                                    (select 
+                                                        host_name, 
+                                                        max(ts_rounded_15_secs) max_ts_rounded_15_secs
+                                                    from 
+                                                        p_threadinfo_delta
+                                                    where
+                                                        ts_rounded_15_secs >= date''#v_from#'' - 1
+                                                    group by host_name)
+
         group by
-	        cpu_usage_ts_rounded_15_secs,
-	        cpu_usage_host_name,
+            tho.ts_rounded_15_secs,
+            tho.host_name,
             pc.process_name,
             pc.process_class,
             tableau_process
-		';
-
-		v_sql := replace(v_sql, '#p_from#', p_from);
-		v_sql := replace(v_sql, '#v_to#', v_to);
-
-		raise notice 'I: %', v_sql;
-		execute v_sql;
-
-		GET DIAGNOSTICS v_num_inserted = ROW_COUNT;
-
-		return v_num_inserted;
+        ) a
+    where
+        a.max_tho_p_id > (select max(coalesce(max_tho_p_id, 0))
+                          from
+                            p_process_class_agg_report
+                          where
+                            ts_rounded_15_secs >= date''#v_from#'' - 1
+                            and a.host_name = host_name
+                         )
+    ';
+                
+    v_sql := replace(v_sql, '#v_from#', v_from);
+    raise notice 'I: %', v_sql;
+        
+    execute v_sql;
+    GET DIAGNOSTICS v_num_inserted = ROW_COUNT;
+	return v_num_inserted;
 END;
 $$ LANGUAGE plpgsql;
--- functions/manage_partitions.sql
+++ functions/manage_partitions.sql
@@ -4,7 +4,7 @@ BEGIN
 		
 		execute 'set search_path = ' || p_schema_name;
 		
-		if lower(p_table_name) in ('threadinfo', 'serverlogs', 'p_serverlogs', 'p_threadinfo', 'p_cpu_usage', 'p_cpu_usage_report') then
+		if lower(p_table_name) in ('threadinfo', 'serverlogs', 'p_serverlogs', 'p_threadinfo', 'p_threadinfo_delta', 'p_cpu_usage', 'p_cpu_usage_report') then
 			perform manage_multi_range_partitions(p_schema_name, p_table_name);
 		elsif lower(p_table_name) in ('plainlogs', 'p_cpu_usage_agg_report', 'p_interactor_session', 'p_process_class_agg_report', 'p_cpu_usage_bootstrap_rpt', 'p_serverlogs_bootstrap_rpt') then
 			perform manage_single_range_partitions(p_schema_name, p_table_name);
