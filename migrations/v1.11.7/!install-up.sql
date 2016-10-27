8250b7bd7db8a212e0c5150928c870ce98cd4079
Gergely Kovacs <kovacsg@starschema.net>
2016-10-27 13:21:26 +0200

    Change version number from 1.11.6 to 1.11.7

--- migrations/v1.11.7/!install-up.sql
+++ migrations/v1.11.7/!install-up.sql
@@ -7,6 +7,6 @@ BEGIN;
 \i 001-up-get_max_ts_by_host.sql
 \i 002-up-load_p_threadinfo.sql
 
-insert into db_version_meta(version_number) values ('v1.11.6');
+insert into db_version_meta(version_number) values ('v1.11.7');
 
 COMMIT;
