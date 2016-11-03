726aa2353e3fd2ef327f0cca47adfebc24288cc0
Gergely Kovacs <kovacsg@starschema.net>
2016-11-03 13:03:19 +0100

    Modify *_compressed logic  to operate once in every day

--- functions/load_s_serverlogs_tabproto_compressed.sql
+++ functions/load_s_serverlogs_tabproto_compressed.sql
@@ -1,46 +1,173 @@
-CREATE or replace function load_s_serverlogs_tabproto_compressed(p_schema_name text) returns bigint
+CREATE or replace function load_s_serverlogs_tabproto_compressed(p_schema_name text, p_load_date date) returns bigint
 AS $$
 declare
 	v_sql text;
 	v_num_inserted bigint;	
-	v_sql_cur text;
-	v_max_ts_date_p_cpu_usage text;
+    v_load_date_txt text := to_char(p_load_date, 'yyyy-mm-dd');
 begin	
 			
-			v_sql_cur := 'select to_char((select #schema_name#.get_max_ts_date(''#schema_name#'',''p_cpu_usage'')), ''yyyy-mm-dd'')';			
-			v_sql_cur := replace(v_sql_cur, '#schema_name#', p_schema_name);
-		
-			execute v_sql_cur into v_max_ts_date_p_cpu_usage;
-			v_max_ts_date_p_cpu_usage := 'date''' || v_max_ts_date_p_cpu_usage || '''';
-			
-			v_sql := 
-			'insert into #schema_name#.s_serverlogs_compressed (
-				  process_name,
-				  host_name,
-				  process_id,
-				  thread_id,	  
-				  session,
-				  ts_cluster,
-				  session_start_ts,  
-				  session_end_ts,
-				  duration,
-				  site,
-				  username,
-				  parent_vizql_session,
-				  parent_vizql_destroy_sess_ts,
-				  parent_dataserver_session,
-				  spawned_by_parent_ts,
-				  parent_process_type,
-				  parent_vizql_site,
-				  parent_vizql_username,
-				  parent_dataserver_site,
-				  parent_dataserver_username,
-				  whole_session_start_ts,
-				  whole_session_end_ts,
-				  whole_session_duration				  
-			)
+    execute 'set local search_path = ' || p_schema_name;            
+						
+	v_sql := 
+	'insert into s_serverlogs_compressed (
+		  process_name,
+		  host_name,
+		  process_id,
+		  thread_id,	  
+		  session,
+		  ts_cluster,
+		  session_start_ts,  
+		  session_end_ts,
+		  duration,
+		  site,
+		  username,
+		  parent_vizql_session,
+		  parent_vizql_destroy_sess_ts,
+		  parent_dataserver_session,
+		  spawned_by_parent_ts,
+		  parent_process_type,
+		  parent_vizql_site,
+		  parent_vizql_username,
+		  parent_dataserver_site,
+		  parent_dataserver_username,
+		  whole_session_start_ts,
+		  whole_session_end_ts,
+		  whole_session_duration				  
+	)
 
-			with t_slogs as
+	with t_slogs as
+	(
+	select 
+			host_name,
+			site,
+			username_without_domain,
+			process_id,
+			thread_id,
+			sess,
+			ts,
+			lag(sess) over (partition by parent_process_type, host_name, process_id, thread_id order by ts) as lag_sess,
+			parent_vizql_session,
+			parent_vizql_destroy_sess_ts,
+			parent_dataserver_session,
+			spawned_by_parent_ts,
+			parent_process_type,
+			parent_vizql_site,
+			parent_vizql_username,
+			parent_dataserver_site,
+			parent_dataserver_username,
+			session_start_ts_utc,
+			session_end_ts_utc					
+	from
+		(select										
+			host_name,
+			site,
+			username_without_domain,
+			process_id,
+			thread_id,
+			sess,
+			ts,
+			parent_vizql_session,
+			parent_vizql_destroy_sess_ts,
+			parent_dataserver_session,
+			spawned_by_parent_ts,
+			parent_process_type,
+			parent_vizql_site,
+			parent_vizql_username,
+			parent_dataserver_site,
+			parent_dataserver_username,
+			row_number() over (partition by parent_process_type,
+											host_name,
+											process_id,
+											thread_id,																
+											ts
+								order by 
+										case when sess not in (''-'', ''default'') then 1 else 0 end 
+										desc, sess desc, site desc) as rn,
+			session_start_ts_utc,
+			session_end_ts_utc
+		from
+				(select distinct 
+							host_name,
+							site,
+							username_without_domain,
+							process_id,
+							-1 as thread_id,									
+							ts,
+							case parent_process_type
+								when ''vizqlserver'' then parent_vizql_session
+								when ''dataserver'' then parent_dataserver_session
+							end as sess,									
+							parent_vizql_session,
+							parent_vizql_destroy_sess_ts,
+							parent_dataserver_session,
+							spawned_by_parent_ts,
+							parent_process_type,
+							parent_vizql_site,
+						    parent_vizql_username,
+						    parent_dataserver_site,
+						    parent_dataserver_username,
+							session_start_ts_utc,
+							session_end_ts_utc
+				from
+						p_serverlogs
+				where
+                    1 = 1
+					and process_name = ''tabprotosrv''
+                    and ts >= date''#v_load_date_txt#'' - interval''60 minutes''
+                    and ts <= date''#v_load_date_txt#'' + interval''26 hours''
+				 ) slogs
+		) a
+	where
+		 rn = 1
+	)
+		  
+	select	
+			''tabprotosrv'' as process_name,
+			host_name,		
+			process_id,
+			thread_id,
+			sess,					
+			ts_claster,
+			min(ts) as session_start_ts,
+			max(ts) as session_end_ts,
+			max(ts) - min(ts) as duration,
+			site,
+			username_without_domain,
+			parent_vizql_session,
+		    parent_vizql_destroy_sess_ts,
+		    parent_dataserver_session,
+		    spawned_by_parent_ts,
+		    parent_process_type,
+			parent_vizql_site,
+			parent_vizql_username,
+			parent_dataserver_site,
+			parent_dataserver_username,
+			whole_session_start_ts,
+			whole_session_end_ts,
+			whole_session_end_ts - whole_session_start_ts as whole_session_duration
+	from
+	(
+		select	
+				host_name,
+				site,
+				username_without_domain,
+				process_id,
+				thread_id,
+				sess,
+				ts,						
+				ts_claster,
+				parent_vizql_session,
+			    parent_vizql_destroy_sess_ts,
+			    parent_dataserver_session,
+			    spawned_by_parent_ts,
+			    parent_process_type,
+				parent_vizql_site,
+				parent_vizql_username,
+				parent_dataserver_site,
+				parent_dataserver_username,
+				session_start_ts_utc as whole_session_start_ts,
+				session_end_ts_utc as whole_session_end_ts
+		from
 			(
 			select 
 					host_name,
@@ -49,190 +176,58 @@ begin
 					process_id,
 					thread_id,
 					sess,
-					ts,
-					lag(sess) over (partition by parent_process_type, host_name, process_id, thread_id order by ts) as lag_sess,
+					ts,							
+					sum(case when sess = lag_sess
+							then 
+								0
+							else 
+								1
+						end	
+						) over (PARTITION BY parent_process_type, host_name, process_id, thread_id, sess order by ts ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) as ts_claster,
 					parent_vizql_session,
-					parent_vizql_destroy_sess_ts,
-					parent_dataserver_session,
-					spawned_by_parent_ts,
-					parent_process_type,
+				    parent_vizql_destroy_sess_ts,
+				    parent_dataserver_session,
+				    spawned_by_parent_ts,
+				    parent_process_type,
 					parent_vizql_site,
 					parent_vizql_username,
 					parent_dataserver_site,
 					parent_dataserver_username,
-					session_start_ts_utc,
-					session_end_ts_utc					
-			from
-				(select										
-					host_name,
-					site,
-					username_without_domain,
-					process_id,
-					thread_id,
-					sess,
-					ts,
-					parent_vizql_session,
-					parent_vizql_destroy_sess_ts,
-					parent_dataserver_session,
-					spawned_by_parent_ts,
-					parent_process_type,
-					parent_vizql_site,
-					parent_vizql_username,
-					parent_dataserver_site,
-					parent_dataserver_username,
-					row_number() over (partition by parent_process_type,
-													host_name,
-													process_id,
-													thread_id,																
-													ts
-										order by 
-												case when sess not in (''-'', ''default'') then 1 else 0 end 
-												desc, sess desc, site desc) as rn,
 					session_start_ts_utc,
 					session_end_ts_utc
-				from
-						(select distinct 
-									host_name,
-									site,
-									username_without_domain,
-									process_id,
-									-1 as thread_id,									
-									ts,
-									case parent_process_type
-										when ''vizqlserver'' then parent_vizql_session
-										when ''dataserver'' then parent_dataserver_session
-									end as sess,									
-									parent_vizql_session,
-									parent_vizql_destroy_sess_ts,
-									parent_dataserver_session,
-									spawned_by_parent_ts,
-									parent_process_type,
-									parent_vizql_site,
-								    parent_vizql_username,
- 								    parent_dataserver_site,
- 								    parent_dataserver_username,
-									session_start_ts_utc,
-									session_end_ts_utc
-						from
-								#schema_name#.p_serverlogs
-						where
-								substr(filename, 1, 11) = ''tabprotosrv'' and
-								ts >= #v_max_ts_date_p_cpu_usage# - interval''60 minutes''
-						 ) slogs
-				) a
-			where
-				 rn = 1
-			)
-				  
-			select	
-					''tabprotosrv'' as process_name,
-					host_name,		
-					process_id,
-					thread_id,
-					sess,					
-					ts_claster,
-					min(ts) as session_start_ts,
-					max(ts) as session_end_ts,
-					max(ts) - min(ts) as duration,
-					site,
-					username_without_domain,
-					parent_vizql_session,
-				    parent_vizql_destroy_sess_ts,
-				    parent_dataserver_session,
-				    spawned_by_parent_ts,
-				    parent_process_type,
-					parent_vizql_site,
-					parent_vizql_username,
-					parent_dataserver_site,
-					parent_dataserver_username,
-					whole_session_start_ts,
-					whole_session_end_ts,
-					whole_session_end_ts - whole_session_start_ts as whole_session_duration
 			from
-			(
-				select	
-						host_name,
-						site,
-						username_without_domain,
-						process_id,
-						thread_id,
-						sess,
-						ts,						
-						ts_claster,
-						parent_vizql_session,
-					    parent_vizql_destroy_sess_ts,
-					    parent_dataserver_session,
-					    spawned_by_parent_ts,
-					    parent_process_type,
-						parent_vizql_site,
-						parent_vizql_username,
-						parent_dataserver_site,
-						parent_dataserver_username,
-						session_start_ts_utc as whole_session_start_ts,
-						session_end_ts_utc as whole_session_end_ts
-				from
-					(
-					select 
-							host_name,
-							site,
-							username_without_domain,
-							process_id,
-							thread_id,
-							sess,
-							ts,							
-							sum(case when sess = lag_sess
-									then 
-										0
-									else 
-										1
-								end	
-								) over (PARTITION BY parent_process_type, host_name, process_id, thread_id, sess order by ts ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) as ts_claster,
-							parent_vizql_session,
-						    parent_vizql_destroy_sess_ts,
-						    parent_dataserver_session,
-						    spawned_by_parent_ts,
-						    parent_process_type,
-							parent_vizql_site,
-							parent_vizql_username,
-							parent_dataserver_site,
-							parent_dataserver_username,
-							session_start_ts_utc,
-							session_end_ts_utc
-					from
-						t_slogs
-					) a	
-				) g
-			group by
-					parent_process_type,
-					spawned_by_parent_ts,
-					host_name,
-					site,
-					username_without_domain,
-					process_id,
-					thread_id,
-					sess,
-					ts_claster,
-					parent_vizql_session,
-				    parent_vizql_destroy_sess_ts,
-				    parent_dataserver_session,
-				    spawned_by_parent_ts,
-				    parent_process_type,
-					parent_vizql_site,
-					parent_vizql_username,
-					parent_dataserver_site,
-					parent_dataserver_username,
-					whole_session_start_ts,
-					whole_session_end_ts
-			';
-
+				t_slogs
+			) a	
+		) g
+	group by
+			parent_process_type,
+			spawned_by_parent_ts,
+			host_name,
+			site,
+			username_without_domain,
+			process_id,
+			thread_id,
+			sess,
+			ts_claster,
+			parent_vizql_session,
+		    parent_vizql_destroy_sess_ts,
+		    parent_dataserver_session,
+		    spawned_by_parent_ts,
+		    parent_process_type,
+			parent_vizql_site,
+			parent_vizql_username,
+			parent_dataserver_site,
+			parent_dataserver_username,
+			whole_session_start_ts,
+			whole_session_end_ts
+	';
 		
-		v_sql := replace(v_sql, '#schema_name#', p_schema_name);
-		v_sql := replace(v_sql, '#v_max_ts_date_p_cpu_usage#', v_max_ts_date_p_cpu_usage);
-				
-		raise notice 'I: %', v_sql;
+	v_sql := replace(v_sql, '#v_load_date_txt#', v_load_date_txt);
+			
+	raise notice 'I: %', v_sql;
 
-		execute v_sql;		
-		GET DIAGNOSTICS v_num_inserted = ROW_COUNT;			
-		return v_num_inserted;
+	execute v_sql;		
+	GET DIAGNOSTICS v_num_inserted = ROW_COUNT;			
+	return v_num_inserted;
 END;
 $$ LANGUAGE plpgsql;
