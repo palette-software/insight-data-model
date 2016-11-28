7f1275475f08d695a9c8b9e38fda1d3d89cd8126
Gergely Kovacs <kovacsg@starschema.net>
2016-11-28 09:23:51 +0100

    Delete current date from p_background_jobs in install-up

--- migrations/v1.12.4/!install-up.sql
+++ migrations/v1.12.4/!install-up.sql
@@ -12,6 +12,9 @@ drop function load_p_background_jobs(text);
 \i 001-up-create_load_p_background_jobs.sql
 select create_load_p_background_jobs('#schema_name#');
 
+delete from p_background_jobs
+where updated_at >= (select max(updated_at)::date from p_background_jobs)
+
 insert into db_version_meta(version_number) values ('v1.12.4');
 
 COMMIT;
