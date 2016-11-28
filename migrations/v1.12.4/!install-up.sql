60606dcde2c410490ea46edbf542a973bd4db7dd
Gergely Kovacs <kovacsg@starschema.net>
2016-11-28 09:13:14 +0100

    Prepare v1.12.4

--- migrations/v1.12.4/!install-up.sql
+++ migrations/v1.12.4/!install-up.sql
@@ -9,7 +9,7 @@ alter table p_background_jobs add column publisher_id int default null;
 alter table p_background_jobs add column p_cre_date timestamp default now();
 
 drop function load_p_background_jobs(text);
-\i create_load_p_background_jobs.sql
+\i 001-up-create_load_p_background_jobs.sql
 select create_load_p_background_jobs('#schema_name#');
 
 insert into db_version_meta(version_number) values ('v1.12.4');
