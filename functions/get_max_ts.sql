adc57e5171f9001ce0f6751c05c91065c7ffd11e
Gergely Kovacs <kovacsg@starschema.net>
2016-12-07 12:28:32 +0100

    Create load_s_tde_filename_pids

--- functions/get_max_ts.sql
+++ functions/get_max_ts.sql
@@ -13,6 +13,7 @@ begin
 						when p_table_name = 'p_cpu_usage_agg_report' then 'timestamp_utc'
 						when p_table_name in ('p_interactor_session', 'p_desktop_session') then 'session_start_ts'
                         when p_table_name in ('p_http_requests', 'p_background_jobs', 'p_async_jobs') then 'created_at'
+                        when p_table_name in ('s_tde_filename_pids') then 'ts_from'
 						else 'ts'
 					end;		
 		for rec in (select
--- tables/s_tde_filename_pids.sql
+++ tables/s_tde_filename_pids.sql
@@ -1,13 +1,15 @@
-CREATE TABLE s_tde_filename_pids 
-	(host_name text,
+CREATE TABLE s_tde_filename_pids (
+    p_id bigserial,
+    host_name text,
 	file_prefix text,
 	pid bigint,
 	ts_from timestamp without time zone,
-	ts_to timestamp without time zone)
+	ts_to timestamp without time zone,
+    p_cre_date timestamp default now()
+    )
 WITH (APPENDONLY=TRUE, ORIENTATION=COLUMN, COMPRESSTYPE=QUICKLZ)
 DISTRIBUTED BY 
 	(host_name,
 	file_prefix,
-	ts_from, 
-	ts_to)
+	ts_from)
 ;
