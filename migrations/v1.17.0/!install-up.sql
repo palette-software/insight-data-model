13a5a99340212533f173f23b208e23689f389d60
Levente Dologh <ldologh@gmail.com>
2016-12-09 09:23:12 +0100

    correct rebase
    The install-up script was moved from v1.16.0 to v1.17.0
    as per my last commit. However, at a conflict we kept
    the file that should stay in the v1.16 dir, so this commit
    copies the script back to v1.16.0, and adds the install script
    of the v1.17.0 migration.

--- migrations/v1.17.0/!install-up.sql
+++ migrations/v1.17.0/!install-up.sql
@@ -4,17 +4,18 @@ set role palette_palette_updater;
 
 BEGIN;
 
-alter table p_desktop_session add column datasource_name TEXT default null;
-alter table p_desktop_session add column publisher_id BIGINT default null;
-alter table p_desktop_session add column publisher_friendly_name_id TEXT default null;
-alter table p_desktop_session add column publisher_user_name_id TEXT default null;
-
-alter table s_desktop_session add column datasource_name TEXT default null;
-alter table s_desktop_session add column publisher_id BIGINT default null;
-alter table s_desktop_session add column publisher_friendly_name_id TEXT default null;
-alter table s_desktop_session add column publisher_user_name_id TEXT default null;
-
-\i 001-up-load_s_desktop_session.sql
+\i 001-up-p_load_dates.sql
+grant select on p_load_dates to palette_palette_looker;
+insert into p_load_dates (load_date) (select max(timestamp_utc) from p_cpu_usage_agg_report);
+drop table p_cpu_usage_agg_report;
+drop function load_s_cpu_usage_agg_report(p_schema_name text, p_load_date date);
+\i 002-up-insert_new_load_date.sql
+drop function manage_partitions(p_schema_name text, p_table_name text);
+\i 003-up-manage_partitions.sql
+drop function manage_single_range_partitions(p_schema_name text, p_table_name text);
+\i 004-up-manage_single_range_partitions.sql
+drop function get_max_ts(p_schema_name text, p_table_name text);
+\i 005-up-get_max_ts.sql
 
 insert into db_version_meta(version_number) values ('v1.17.0');
 
