399756e882f802ff5b1a0d57f60f7d86fdae3b0f
Gergely Kovacs <kovacsg@starschema.net>
2016-12-09 11:30:01 +0100

    Prepare v1.17.0

--- migrations/v1.17.0/!install-up.sql
+++ migrations/v1.17.0/!install-up.sql
@@ -6,10 +6,10 @@ BEGIN;
 
 \i 001-up-p_load_dates.sql
 grant select on p_load_dates to palette_palette_looker;
+\i 002-up-insert_new_load_date.sql
+select insert_new_load_date('#schema_name#', (select max(timestamp_utc)::date from p_cpu_usage_agg_report));
 drop table p_cpu_usage_agg_report;
 drop table s_cpu_usage_agg_report;
-\i 002-up-insert_new_load_date.sql
-select insert_new_load_date('palette', (select max(timestamp_utc)::date from p_cpu_usage_agg_report));
 \i 003-up-manage_partitions.sql
 \i 004-up-manage_single_range_partitions.sql
 \i 005-up-get_max_ts.sql
