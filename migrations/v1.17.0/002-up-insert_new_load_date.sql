45cd4dbbc61b021598137ec35d4ef8f5b33652b4
Levente Dologh <ldologh@gmail.com>
2016-12-09 09:36:08 +0100

    simplify insert_new_load_date function

--- migrations/v1.17.0/002-up-insert_new_load_date.sql
+++ migrations/v1.17.0/002-up-insert_new_load_date.sql
@@ -1,18 +1,14 @@
 CREATE or replace function insert_new_load_date(p_schema_name text, p_load_date date) returns bigint
 AS $$
 declare
-	v_sql text;
 	v_num_inserted bigint;
 begin		
 
 	execute 'set local search_path = ' || p_schema_name;
 
-	perform check_if_load_date_already_in_table(p_schema_name, 'load_dates', p_load_date, false);
+	perform check_if_load_date_already_in_table(p_schema_name, 'p_load_dates', p_load_date, false);
 
-	v_sql := 'insert into p_load_dates (p_load_date) values (' || p_load_date || ')';
-
-	raise notice 'I: %', v_sql;
-	execute v_sql;
+	insert into p_load_dates (load_date) values (p_load_date);
 
 	GET DIAGNOSTICS v_num_inserted = ROW_COUNT;
 	return v_num_inserted;
