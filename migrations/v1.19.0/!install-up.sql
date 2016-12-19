c07b14ab0562eceb81989cb38825787b0796bb00
Levente Dologh <ldologh@gmail.com>
2016-12-19 13:33:15 +0100

    fix install-up script

--- migrations/v1.19.0/!install-up.sql
+++ migrations/v1.19.0/!install-up.sql
@@ -5,9 +5,9 @@ set role palette_palette_updater;
 BEGIN;
 
 drop table p_threadinfo;
-drop function load_p_threadinfo_delta(p_schema_name text);
+drop function create_p_threadinfo_delta(p_schema_name text);
 alter table p_threadinfo_delta rename to p_threadinfo;
-alter function load_p_threadinfo_delta rename to load_p_threadinfo
+alter function load_p_threadinfo_delta(p_schema_name text) rename to load_p_threadinfo;
 \i 001-up-get_max_ts.sql
 \i 002-up-load_s_cpu_usage_dataserver.sql
 \i 003-up-load_s_cpu_usage_rest.sql
