feb7b36fb4cd34f4c53b3e07fb05ab2bb3f6f8cc
Gergely Kovacs <kovacsg@starschema.net>
2018-03-10 14:22:53 +0100

    List cols in select list for V1.19.2

--- migrations/v1.19.2/006-up-copy_p_src_logs_bootstrap.sql
+++ migrations/v1.19.2/006-up-copy_p_src_logs_bootstrap.sql
@@ -24,14 +24,113 @@ BEGIN
               fetch c into rec;
               exit when not found;
 
-              v_sql := 'insert into p_serverlogs_bootstrap_rpt
-                        select *
-                        from
-                            p_serverlogs_bootstrap_rpt_orig
-                        where 1 = 1
-                            and start_ts >= date''#load_day#''
-                            and start_ts < date''#load_day#'' + interval''1 day''
-                        ';              
+              v_sql := 'insert into p_serverlogs_bootstrap_rpt (
+                            p_id
+                           ,p_serverlogs_p_id
+                           ,serverlogs_id
+                           ,p_filepath
+                           ,filename
+                           ,process_name
+                           ,host_name
+                           ,ts
+                           ,process_id
+                           ,thread_id
+                           ,sev
+                           ,req
+                           ,sess
+                           ,site
+                           ,username
+                           ,username_without_domain
+                           ,k
+                           ,v
+                           ,parent_vizql_session
+                           ,parent_vizql_destroy_sess_ts
+                           ,parent_dataserver_session
+                           ,spawned_by_parent_ts
+                           ,parent_process_type
+                           ,parent_vizql_site
+                           ,parent_vizql_username
+                           ,parent_dataserver_site
+                           ,parent_dataserver_username
+                           ,p_serverlogs_p_cre_date
+                           ,thread_name
+                           ,elapsed_ms
+                           ,start_ts
+                           ,session_start_ts_utc
+                           ,session_end_ts_utc
+                           ,site_name_id
+                           ,project_name_id
+                           ,workbook_name_id
+                           ,workbook_rev
+                           ,publisher_username_id
+                           ,user_type
+                           ,session_duration
+                           ,session_elapsed_seconds
+                           ,currentsheet
+                           ,p_cre_date
+                           ,publisher_id
+                           ,site_id
+                           ,project_id
+                           ,workbook_id
+                           ,view_id
+                           ,v_truncated
+                     )                       
+                    select
+                        p_id
+                       ,p_serverlogs_p_id
+                       ,serverlogs_id
+                       ,p_filepath
+                       ,filename
+                       ,process_name
+                       ,host_name
+                       ,ts
+                       ,process_id
+                       ,thread_id
+                       ,sev
+                       ,req
+                       ,sess
+                       ,site
+                       ,username
+                       ,username_without_domain
+                       ,k
+                       ,v
+                       ,parent_vizql_session
+                       ,parent_vizql_destroy_sess_ts
+                       ,parent_dataserver_session
+                       ,spawned_by_parent_ts
+                       ,parent_process_type
+                       ,parent_vizql_site
+                       ,parent_vizql_username
+                       ,parent_dataserver_site
+                       ,parent_dataserver_username
+                       ,p_serverlogs_p_cre_date
+                       ,thread_name
+                       ,elapsed_ms
+                       ,start_ts
+                       ,session_start_ts_utc
+                       ,session_end_ts_utc
+                       ,site_name_id
+                       ,project_name_id
+                       ,workbook_name_id
+                       ,workbook_rev
+                       ,publisher_username_id
+                       ,user_type
+                       ,session_duration
+                       ,session_elapsed_seconds
+                       ,currentsheet
+                       ,p_cre_date
+                       ,publisher_id
+                       ,site_id
+                       ,project_id
+                       ,workbook_id
+                       ,view_id
+                       ,v_truncated                        
+                    from
+                        p_serverlogs_bootstrap_rpt_orig
+                    where 1 = 1
+                        and start_ts >= date''#load_day#''
+                        and start_ts < date''#load_day#'' + interval''1 day''
+                    ';              
               v_sql := replace(v_sql, '#load_day#', to_char(rec.load_day, 'yyyy-mm-dd'));
               execute v_sql;  
         end loop;
